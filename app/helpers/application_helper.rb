6689c1c49460e4543b24b2c1160344cf39d63f90
Hein Dauven <heindauven@gmail.com>
2016-09-22 13:43:25 +0200

    Added controller logic and attached the views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Method that adds an 'active' class to the 'li' tag if the current page equals the menu item.
+  # <%= link_to_active "Home", root_path, id: "home_link" %>
+  def link_to_active(body, url, html_options = {})
+    active = "active" if current_page?(url)
+    content_tag :li, class: active do
+      link_to body, url, html_options
+    end
+  end
 end
